0a7701197b34d23858e072c524fec7380d0f0325
Matheus Sampaio <matheus@sampaio.us>
2021-01-12 13:39:39 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <network
+    class="network"
+    :nodes="nodes"
+    :edges="edges"
+    :options="options"
+    >
+  </network>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Network } from 'vue-visjs'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Network
+  },
+  data() {
+    return {
+      id: 0,
+      label: '',
+      from: 0,
+      to: 0,
+      nodes: [
+      ],
+      edges: [
+      ]
+    }
+  },
+  methods: {
+    addNode() {
+      this.nodes.push({ id: this.id, label: this.label })
+    },
+    addEdge() {
+      this.edges.push({ from: this.from, to: this.to })
+    }
+  },
+  computed: {
+    options() {
+      return {
+        autoResize: true,
+        height: '100%',
+        width: '100%',
+        manipulation: {
+          enabled: true
+        }
+      }
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+}
+
+.network {
+  height: 100vh;
 }
 </style>
---
+++ src/components/Network.vue
@@ -0,0 +1,32 @@
+<template>
+</template>
+
+<script>
+import { Network } from 'vue-visjs'
+
+export default {
+  components: {
+    Network
+  },
+  props: {
+    nodes: {
+      type: Array,
+      default: () => []
+    },
+    edges: {
+      type: Array,
+      default: () => []
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+  }
+}
+</script>
+
+<style scoped>
+.network {
+  height: 100vh;
+}
+</style>
