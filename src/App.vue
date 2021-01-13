a328002084ad38a09d9b03036560b059ffd8d97f
Matheus Sampaio <matheus@sampaio.us>
2021-01-12 18:41:29 -0600

    feat: add labels

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,135 @@
 <template>
-  <network :nodes="nodes" :links="links">
-  </network>
+  <div>
+    <div>
+      <input type="text" v-model="source" placeholder="source">
+      <input type="text" v-model="target" placeholder="target">
+      <input type="number" v-model="timeToClose" placeholder="target">
+      <select v-model="size">
+        <option :value="'small'">Small</option>
+        <option :value="'medium'">Medium</option>
+        <option :value="'large'">Large</option>
+      </select>
+      <button @click="addLink">Add Link</button>
+      <button @click="clear">Clear</button>
+    </div>
+    <network :nodes="nodes" :links="links" :width="width" :height="height"></network>
+  </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 import Network from './components/Network.vue'
 
 export default {
+  name: 'App',
   components: {
     Network
   },
   data() {
     return {
       nodes: [
-        { id: "12345" },
-        { id: "67890" },
-        { id: "45678" }
       ],
       links: [
-        { source: "12345", target: "67890" },
-        { source: "67890", target: "45678" },
-        { source: "45678", target: "12345" }
-      ]
+      ],
+      source: '',
+      target: '',
+      timeToClose: 5,
+      size: 'small',
+      interval: null,
+      width: window.innerWidth - 30,
+      height: window.innerHeight - 100,
     }
+  },
+  methods: {
+    clear() {
+      this.nodes = []
+      this.links = []
+
+      localStorage.removeItem('nodes')
+      localStorage.removeItem('links')
+    },
+    addLink() {
+      const sourceExists = this.nodes.find(n => n.id === this.source)
+
+      if (!sourceExists) {
+        this.nodes.push({ id: this.source })
+      }
+
+      const targetExists = this.nodes.find(n => n.id === this.target)
+
+      if (!targetExists) {
+        this.nodes.push({ id: this.target })
+      }
+
+      if (!sourceExists || !targetExists) {
+        localStorage.setItem("nodes", JSON.stringify(this.nodes))
+      }
+
+      this.links = this.links.filter(l => l.source !== this.source || l.target !== this.target)
+
+      let color = 'green'
+
+      if (this.size === 'small') {
+        color = 'green' 
+      }
+
+      if (this.size === 'medium') {
+        color = 'blue' 
+      }
+
+      if (this.size === 'large') {
+        color = 'yellow' 
+      }
+
+      this.links.push({
+        color,
+        source: this.source,
+        target: this.target,
+        timeToClose: moment().add(this.timeToClose, 'hours').toISOString()
+      })
+
+      localStorage.setItem("links", JSON.stringify(this.links))
+    },
+    update() {
+      const now = moment.now()
+
+      this.links = this.links.filter(l => moment(l.timeToClose).isAfter(now))
+
+      localStorage.setItem("links", JSON.stringify(this.links))
+    },
+    onResize() {
+      this.height = window.innerHeight - 100
+      this.width = window.innerWidth - 30
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('links')) {
+      this.links = JSON.parse(localStorage.getItem('links'));
+    }
+
+    if (localStorage.getItem('nodes')) {
+      this.nodes = JSON.parse(localStorage.getItem('nodes'));
+    }
+
+    if (this.interval == null) {
+      this.update()
+
+      this.interval = setInterval(() => this.update(), 5 * 60 * 1000)
+    }
+
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.onResize);
+    })
+  },
+  beforeDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval)
+
+      this.interval = null
+    }
+
+    window.removeEventListener('resize', this.onResize); 
   }
 }
 </script>
--- src/components/Network.vue
+++ src/components/Network.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+import moment from 'moment'
 import * as d3 from "d3"
 
 export default {
@@ -17,47 +18,61 @@ export default {
     links: {
       type: Array,
       default: () => []
+    },
+    height: {
+      type: Number,
+      default: 500
+    },
+    width: {
+      type: Number,
+      default: 500
+    }
+  },
+  data() {
+    return {
+      interval: null
     }
   },
   computed: {
-    chart() {
+    graph() {
       const chart = (() => {
-        const height = 500
-        const width = 500
-
-        const color = d3.scaleOrdinal(d3.schemeTableau10)
-
         const svg = d3.select("#svg")
-          .attr("width", width)
-          .attr("height", height)
-          .attr("viewBox", [-width / 2, -height / 2, width, height]);
+          .attr("width", '100%')
+          .attr("height", '100%')
+          .attr("viewBox", [-this.width / 2, -this.height / 2, this.width, this.height])
+          .call(d3.zoom().on("zoom", () => {
+            console.log('zoom')
+            svg.attr("transform", d3.event.transform)
+          }))
+        
+        const container = svg.append("g")
+          .attr("id", "container")
+          .call(d3.zoom().on("zoom", () => {
+            console.log('zoom')
+            container.attr("transform", d3.event.transform)
+          }))
 
         const simulation = d3.forceSimulation()
           .force("charge", d3.forceManyBody().strength(-1000))
           .force("link", d3.forceLink().id(d => d.id).distance(200))
           .force("x", d3.forceX())
           .force("y", d3.forceY())
-          .on("tick", ticked);
+          .on("tick", ticked)
 
-        let link = svg.append('g')
+        let link = container.append('g')
           .selectAll(".link")
-          // .data(this.links)
-          // .enter()
           .append("line")
           .attr("class", "link")
-          // .attr("stroke", "#000")
-          // .attr("stroke-width", 1.5)
-          // .selectAll("line");
+        
+        let linkLabel = container.append('g')
+          .selectAll('.link-label')
+          .append('text')
+          .attr("class", "link-label")
 
-        let node = svg.append('g')
+        let node = container.append('g')
           .selectAll('.node')
-          // .data(this.nodes)
-          // .enter()
           .append('g')
           .attr("class", "node")
-          // .attr("stroke", "#fff")
-          // .attr("stroke-width", 1.5)
-          // .selectAll("circle")
           
         function ticked() {
           link.attr("x1", d => d.source.x)
@@ -65,20 +80,18 @@ export default {
             .attr("x2", d => d.target.x)
             .attr("y2", d => d.target.y)
 
-          node.attr("transform", d => "translate(" + d.x + "," + d.y + ")")
+          linkLabel.attr("transform", d => `translate(${(d.source.x + d.target.x) / 2}, ${(d.source.y + d.target.y) / 2})`)
+          node.attr("transform", d => `translate(${d.x},${d.y})`)
         }
-
-        // Terminate the force layout when this cell re-runs.
-        // invalidation.then(() => simulation.stop());
 
         return Object.assign(svg.node(), {
           update({ nodes, links }) {
             // Make a shallow copy to protect against mutation, while
             // recycling old nodes to preserve position and velocity.
-            const old = new Map(node.data().map(d => [d.id, d]));
+            const old = new Map(node.data().map(d => [d.id, d]))
 
-            nodes = nodes.map(d => Object.assign(old.get(d.id) || {}, d));
-            links = links.map(d => Object.assign({}, d));
+            nodes = nodes.map(d => Object.assign(old.get(d.id) || {}, d))
+            links = links.map(d => Object.assign({}, d))
 
             node = node
               .data(nodes, d => d.id)
@@ -88,11 +101,12 @@ export default {
 
                 group.append("circle")
                     .attr("r", 8)
-                    .attr("fill", d => color(d.id))
+                    .attr("fill", d => d.color || 'black')
 
                 group.append("text")
                     .attr("dx", d => (d.id.length / 2) * -10)
                     .attr("dy", 25)
+                    .attr('fill', d => d.textColor || 'black') 
                     .text(d => d.id)
 
                 return group
@@ -103,13 +117,23 @@ export default {
               .join(enter => enter
                 .append('line')
                 .attr('class', 'link')
+                .attr('stroke', d => d.color || 'black') 
+                .attr('id', d => `${d.source}->${d.target}`)
+              )
+            
+            linkLabel = linkLabel
+              .data(links, d => [d.source, d.target])
+              .join(enter => enter
+                .append('text')
+                .attr('class', 'link-label')
+                .text(d => moment(d.timeToClose).fromNow())
               )
 
-            simulation.nodes(nodes);
-            simulation.force("link").links(links);
-            simulation.alpha(1).restart();
+            simulation.nodes(nodes)
+            simulation.force("link").links(links)
+            simulation.alpha(1).restart()
           }
-        });
+        })
       })()
 
       return chart
@@ -117,7 +141,9 @@ export default {
   },
   methods: {
     update() {
-      this.chart.update({
+      console.log('updating graph')
+
+      this.graph.update({
         nodes: this.nodes,
         links: this.links
       })
@@ -132,7 +158,16 @@ export default {
     }
   },
   mounted() {
-    this.update()
+    if (this.interval == null) {
+      this.interval = setInterval(() => this.update(), 5 * 60 * 1000)
+    }
+  },
+  destoyed() {
+    if (this.interval) {
+      clearInterval(this.interval)
+
+      this.interval = null
+    }
   }
 }
 </script>
@@ -142,13 +177,8 @@ svg {
   border: 1px solid black;
 }
 
-.link {
-  stroke: #888888;
-}
-
 .node text {
   pointer-events: none;
   font-size: 20px;
-  color: black;
 }
 </style>
